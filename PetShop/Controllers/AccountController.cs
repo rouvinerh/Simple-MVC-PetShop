8f93164ed34015b2b189fd1e8fe835bef26e4e11
Rouvin <rouvinerh@gmail.com>
2024-03-10 16:00:02 +0800

    Add login model and view

---
+++ PetShop/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetShop.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
---
+++ PetShop/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetShop.Models
+{
+    public class LoginViewModel
+    {
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(60)]
+        public string? Username { get; set; }
+
+        [Required, StringLength(60)]
+        public string? Password { get; set; }
+    }
+}
