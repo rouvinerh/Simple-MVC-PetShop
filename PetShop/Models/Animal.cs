6ba06988438281aa44dbe74631a250acd39420fd
Rouvin <rouvinerh@gmail.com>
2024-03-10 14:56:59 +0800

    Add data validation in Animal Controller

--- PetShop/Models/Animal.cs
+++ PetShop/Models/Animal.cs
@@ -7,18 +7,23 @@ namespace PetShop.Models
     {
         public int Id { get; set; }
 
+        [Range(1, 300), Required]
         public int Age { get; set; }
 
+        [StringLength(60, MinimumLength = 4), Required]
         public string Species { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(6)]
         public string Sex { get; set; }
 
         [Display(Name = "Date Received")]
         [DataType(DataType.Date)]
         public DateTime GetDate { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
         public string Name { get; set; }
 
+        [Range(1, 5000), DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
